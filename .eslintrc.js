f54b0f773e8115f4a11da6b3a42273f5225129fd
theobassan <theobassan@gmail.com>
2021-10-01 15:48:02 -0300

    add max-params rule

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+    plugins: ['no-null'],
+    rules: {
+        'no-null/no-null': 2,
+        '@typescript-eslint/no-unused-vars': 2,
+        'max-params': ['error', { max: 1 }],
+        'max-depth': ['error', { max: 10 }], //2
+        'max-lines': ['error', { max: 150 }], //60
+        'max-lines-per-function': ['error', { max: 300 }], //30
+        'max-nested-callbacks': ['error', { max: 3 }],
+        'max-statements': ['error', { max: 20 }], //10
+        'max-statements-per-line': ['error', { max: 1 }],
+    },
+};
---
+++ src/max-params.ts
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const astUtils = require('eslint/lib/rules/utils/ast-utils');
+const { upperCaseFirst } = require('eslint/lib/shared/string-utils');
+
+const TARGET_METHODS = /^(every|filter|find|flatMap|forEach|map|reduce|some|sort)$/;
+
+const isTargetMethod = (node: any) => {
+    const checkNode = astUtils.skipChainExpression(node);
+
+    if (checkNode.type !== 'MemberExpression') {
+        return false;
+    }
+
+    if (!astUtils.isSpecificId(checkNode.property, TARGET_METHODS)) {
+        return false;
+    }
+
+    return true;
+};
+
+const getArrayMethodName = (node: any) => {
+    let currentNode = node;
+
+    while (currentNode) {
+        const parent = currentNode.parent;
+
+        switch (parent.type) {
+            case 'LogicalExpression':
+            case 'ConditionalExpression':
+            case 'ChainExpression':
+                currentNode = parent;
+                break;
+            case 'ReturnStatement': {
+                const func = astUtils.getUpperFunction(parent);
+
+                if (func === undefined || !astUtils.isCallee(func)) {
+                    return undefined;
+                }
+                currentNode = func.parent;
+                break;
+            }
+            case 'CallExpression':
+                if (astUtils.isArrayFromMethod(parent.callee)) {
+                    if (parent.arguments.length >= 2 && parent.arguments[1] === currentNode) {
+                        return 'from';
+                    }
+                }
+                if (isTargetMethod(parent.callee)) {
+                    if (parent.arguments.length >= 1 && parent.arguments[0] === currentNode) {
+                        return astUtils.getStaticPropertyName(parent.callee);
+                    }
+                }
+                console.log('isTargetMethod: ', false);
+                return undefined;
+            default:
+                return undefined;
+        }
+    }
+    return undefined;
+};
+
+module.exports = {
+    meta: {
+        type: 'suggestion',
+
+        docs: {
+            description: 'enforce a maximum number of parameters in function definitions except arrays calls',
+            recommended: false,
+        },
+
+        schema: [
+            {
+                oneOf: [
+                    {
+                        type: 'integer',
+                        minimum: 0,
+                    },
+                    {
+                        type: 'object',
+                        properties: {
+                            maximum: {
+                                type: 'integer',
+                                minimum: 0,
+                            },
+                            max: {
+                                type: 'integer',
+                                minimum: 0,
+                            },
+                        },
+                        additionalProperties: false,
+                    },
+                ],
+            },
+        ],
+        messages: {
+            exceed: '{{name}} has too many parameters ({{count}}). Maximum allowed is {{max}}.',
+        },
+    },
+
+    create(context: any) {
+        const sourceCode = context.getSourceCode();
+        const option = context.options[0];
+        let numParams = 3;
+
+        if (
+            typeof option === 'object' &&
+            (Object.prototype.hasOwnProperty.call(option, 'maximum') ||
+                Object.prototype.hasOwnProperty.call(option, 'max'))
+        ) {
+            numParams = option.maximum || option.max;
+        }
+        if (typeof option === 'number') {
+            numParams = option;
+        }
+
+        function checkFunction(node: any) {
+            if (node.params.length > numParams && getArrayMethodName(node) == undefined) {
+                context.report({
+                    loc: astUtils.getFunctionHeadLoc(node, sourceCode),
+                    node,
+                    messageId: 'exceed',
+                    data: {
+                        name: upperCaseFirst(astUtils.getFunctionNameWithKind(node)),
+                        count: node.params.length,
+                        max: numParams,
+                    },
+                });
+            }
+        }
+
+        return {
+            FunctionDeclaration: checkFunction,
+            ArrowFunctionExpression: checkFunction,
+            FunctionExpression: checkFunction,
+        };
+    },
+};
