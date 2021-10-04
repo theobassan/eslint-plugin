eb70715ca95b3b903f6fd2b75167283e85b65201
theobassan <theobassan@gmail.com>
2021-10-04 19:29:36 -0300

    lint config changes

--- src/max-params.ts
+++ src/max-params.ts
@@ -5,25 +5,23 @@ import { create } from './max-params-create';
 module.exports = {
     meta: {
         type: 'suggestion',
-
         docs: {
-            description: 'enforce a maximum number of parameters in function definitions except arrays calls',
+            description: 'enforce a maximum number of parameters in function definitions',
             recommended: false,
         },
-
         schema: [
             {
                 oneOf: [
                     {
                         type: 'integer',
-                        minimum: 0,
+                        minimum: 1,
                     },
                     {
                         type: 'object',
                         properties: {
                             max: {
                                 type: 'integer',
-                                minimum: 0,
+                                minimum: 1,
                             },
                             allowArraysCallbacks: {
                                 type: 'boolean',
@@ -38,7 +36,6 @@ module.exports = {
             exceed: '{{name}} has too many parameters ({{count}}). Maximum allowed is {{max}}.',
         },
     },
-
     create(context: Rule.RuleContext) {
         return create(context);
     },
